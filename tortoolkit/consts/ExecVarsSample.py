35865f9423e117f52ecbb0c1f1f03222a7c29416
Caleb Noble <error@notavailable.live>
2021-03-14 10:26:48 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -1,95 +1,94 @@
-try:
-    from .ExecVars import ExecVars
-except:
-    class ExecVars:
-        # Set true if its VPS
-        IS_VPS = False
-        
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+import os
 
-        # Edit the server port if you want to keep it default though.
-        SERVPORT = 80
+class ExecVars:
+    # Set true if its VPS
+    IS_VPS = False
 
-        # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
-        
-        # Google Drive Index Link should include the base dir also See readme for more info
-        GD_INDEX_URL = False
+    API_HASH = str(os.environ.get("API_HASH", None))
+    API_ID = int(os.environ.get("API_ID", None))
+    BOT_TOKEN = str(os.environ.get("BOT_TOKEN", None))
+    BASE_URL_OF_BOT = os.environ.get("BASE_URL", False)
 
-        # Time to wait before edit message
-        EDIT_SLEEP_SECS = 40
+    # Edit the server port if you want to keep it default though.
+    SERVPORT = os.environ.get("SERVPORT", 80)
 
-        # Telegram Upload Limit (in bytes)
-        TG_UP_LIMIT = 1700000000
+    # ALLOWED USERS [ids of user or supergroup] seperate by commas
+    ALD_USR = f"[{os.environ.get("ALD_USR", None)}]"
 
-        # Should force evething uploaded into Document
-        FORCE_DOCUMENTS = False
+    # Google Drive Index Link should include the base dir also See readme for more info
+    GD_INDEX_URL = bool(os.environ.get("GD_INDEX_URL", False))
 
-        # Chracter to use as a completed progress 
-        COMPLETED_STR = "▰"
+    # Time to wait before edit message
+    EDIT_SLEEP_SECS = int(os.environ.get("EDIT_SLEEP_SECS", False))
 
-        # Chracter to use as a incomplete progress
-        REMAINING_STR = "▱"
+    # Telegram Upload Limit (in bytes)
+    TG_UP_LIMIT = 1700000000
 
-        # DB URI for access
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
-        
-        # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
-        #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
-        
-        # The base direcory to which the files will be upload if using RCLONE
-        RCLONE_BASE_DIR = "/"
+    # Should force evething uploaded into Document
+    FORCE_DOCUMENTS = False
 
-        # This value will be considered only if Rclone is True - this may be defied now ;)
-        # Cuz at least one needs to be Ture at a time either RCLONE or Leech.
-        LEECH_ENABLED = True
+    # Chracter to use as a completed progress 
+    COMPLETED_STR = "▰"
 
-        # Will be enabled once its set
-        # For vps change it to True if config loaded
-        RCLONE_ENABLED = False
+    # Chracter to use as a incomplete progress
+    REMAINING_STR = "▱"
 
-        # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
-        DEFAULT_TIMEOUT = "leech"
+    # DB URI for access
+    DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
 
-        # For vps set path here or you can use runtime too
-        RCLONE_CONFIG = False
-        
-        # Name of the RCLONE drive from the config
-        DEF_RCLONE_DRIVE = ""
+    # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
+    #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
 
-        # Max size of a playlist that is allowed (Number of videos)
-        MAX_YTPLAYLIST_SIZE = 20
-        
-        # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 10
+    # The base direcory to which the files will be upload if using RCLONE
+    RCLONE_BASE_DIR = "/"
 
-        # Set this to your bot username if you want to add the username of your bot at the end of the commands like
-        # /leech@TorToolkitBot so the value will be @TorToolkitBot
-        BOT_CMD_POSTFIX = "" 
+    # This value will be considered only if Rclone is True - this may be defied now ;)
+    # Cuz at least one needs to be Ture at a time either RCLONE or Leech.
+    LEECH_ENABLED = True
 
-        # Time out for the status Delete.
-        STATUS_DEL_TOUT = 20
+    # Will be enabled once its set
+    # For vps change it to True if config loaded
+    RCLONE_ENABLED = False
 
-        # Allow the user settings to be accessed in private
-        USETTINGS_IN_PRIVATE = False
+    # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
+    DEFAULT_TIMEOUT = "leech"
 
-        # This is to stop someone from abusing the system by imposing the limit
-        # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
-        USER_CAP_ENABLE = False
-        USER_CAP_LIMIT = [50,10,2]
+    # For vps set path here or you can use runtime too
+    RCLONE_CONFIG = False
 
-        # No need to worry about these
-        # CHANGE THESE AT YOUR RISK
-        LOCKED_USERS = False
-        RSTUFF = False
-        FORCE_DOCS_USER = False
-        FAST_UPLOAD = True
-        METAINFO_BOT = False
-        EXPRESS_UPLOAD = True
-        
+    # Name of the RCLONE drive from the config
+    DEF_RCLONE_DRIVE = ""
+
+    # Max size of a playlist that is allowed (Number of videos)
+    MAX_YTPLAYLIST_SIZE = 20
+
+    # Max size of the torrent allowed
+    MAX_TORRENT_SIZE = 10
+
+    # Set this to your bot username if you want to add the username of your bot at the end of the commands like
+    # /leech@TorToolkitBot so the value will be @TorToolkitBot
+    BOT_CMD_POSTFIX = "" 
+
+    # Time out for the status Delete.
+    STATUS_DEL_TOUT = 20
+
+    # Allow the user settings to be accessed in private
+    USETTINGS_IN_PRIVATE = False
+
+    # This is to stop someone from abusing the system by imposing the limit
+    # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
+    USER_CAP_ENABLE = False
+    USER_CAP_LIMIT = [50,10,2]
+
+    # No need to worry about these
+    # CHANGE THESE AT YOUR RISK
+    LOCKED_USERS = False
+    RSTUFF = False
+    FORCE_DOCS_USER = False
+    FAST_UPLOAD = True
+    METAINFO_BOT = False
+    EXPRESS_UPLOAD = True
+
 
 
 
